fa8aff23aeb0baeb5fbffe8e1bd72d924966f130
ShalmanAhmed <blackhole01535459800@gmail.com>
2026-09-23 03:52:41 +0600

    Change output mode from 'export' to 'standard'

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
+  output: "standard",
   /* config options here */
   typescript: {
     ignoreBuildErrors: true,
