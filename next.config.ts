d2047036b0bf78d5e28e2f4d573d39dce92fc17d
ShalmanAhmed <blackhole01535459800@gmail.com>
2026-09-23 03:55:33 +0600

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "standard",
+  output: "standalone",
   /* config options here */
   typescript: {
     ignoreBuildErrors: true,
